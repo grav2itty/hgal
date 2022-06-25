ab70cc2e5823915a0dd28eef52829e92b45a0cab
grav2itty <grav2itty@gmail.com>
2022-06-25 19:33:43 +0200

    properties

--- src/Hgal/Data/SurfaceMesh.hs
+++ src/Hgal/Data/SurfaceMesh.hs
@@ -23,54 +23,66 @@ import Hgal.Graph.Loops
 import Debug.Trace
 import qualified Hgal.Graph.EulerOperationsM as Euler
 
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 data family Connectivity a :: *
+
+class Property s k p | k -> p, p -> k, k -> s where
+  type Container k :: * -> *
+
+  -- pGet :: s -> k -> Maybe p
+  -- pAdjust :: (p -> p) -> k -> s -> s
+  property :: k -> Lens' s (Maybe p)
+  properties :: s -> (Container k) p
 
 class Element a where
   data Size a :: *
 
   name :: a -> String
 
-  numElements :: SurfaceMesh v h e f -> Size a
-  new :: SurfaceMesh v h e f -> a
+  numElements :: SurfaceMesh v d -> Size a
+  new :: SurfaceMesh v d -> a
 
   nullE :: a
   default nullE :: Bounded a => a
   nullE = maxBound
 
-  conn :: a -> Lens' (SurfaceMesh v h e f) (Connectivity a)
+  conn :: a -> Lens' (SurfaceMesh v d) (Connectivity a)
 
-  hasValidIndex :: SurfaceMesh v h e f -> a -> Bool
-  hasValidConnectivity :: SurfaceMesh v h e f -> a -> Either String Bool
+  propertyOf :: Property s a p => a -> Lens' (SurfaceMesh v s) (Maybe p)
 
-  isValid :: SurfaceMesh v h e f -> a -> Either String Bool
+  hasValidIndex :: SurfaceMesh v d -> a -> Bool
+  hasValidConnectivity :: SurfaceMesh v d -> a -> Either String Bool
+
+  isValid :: SurfaceMesh v d -> a -> Either String Bool
   isValid sm v
     | hasValidIndex sm v = hasValidConnectivity sm v
     | otherwise = Right False
 
-  isBorder :: SurfaceMesh v h e f -> a -> Bool
+  isBorder :: SurfaceMesh v d -> a -> Bool
 
-  isRemoved :: SurfaceMesh v h e f -> a -> Bool
+  isRemoved :: SurfaceMesh v d -> a -> Bool
   isRemoved _ _ = False
 
-
-  vertex :: SurfaceMesh v h e f -> a -> Vertex
+  vertex :: SurfaceMesh v d -> a -> Vertex
   vertex sm = vertex sm . halfedge sm
 
-  halfedge :: SurfaceMesh v h e f -> a -> Halfedge
+  halfedge :: SurfaceMesh v d -> a -> Halfedge
 
-  setHalfedge :: SurfaceMesh v h e f -> a -> Halfedge -> SurfaceMesh v h e f
+  setHalfedge :: SurfaceMesh v d -> a -> Halfedge -> SurfaceMesh v d
   setHalfedge sm _ _ = sm
 
-  edge :: SurfaceMesh v h e f -> a -> Edge
+  edge :: SurfaceMesh v d -> a -> Edge
   edge sm = (\(Halfedge i) -> Edge i) . halfedge sm
 
-  face :: SurfaceMesh v h e f -> a -> Face
+  face :: SurfaceMesh v d -> a -> Face
   face sm = face sm . halfedge sm
 
-  next :: SurfaceMesh v h e f -> a -> a
-  prev :: SurfaceMesh v h e f -> a -> a
-
+  next :: SurfaceMesh v d -> a -> a
+  prev :: SurfaceMesh v d -> a -> a
 
 newtype Vertex = Vertex Int
     deriving (Bounded, Enum, Eq, Ord)
@@ -85,14 +97,12 @@ newtype Face = Face Int
     deriving (Bounded, Enum, Eq, Ord)
     deriving newtype (Bits, Integral, Num, Real)
 
-data SurfaceMesh v h e f = SurfaceMesh
+data SurfaceMesh v d = SurfaceMesh
   { _vconn :: Seq (Connectivity Vertex)
   , _hconn :: Seq (Connectivity Halfedge)
   , _fconn :: Seq (Connectivity Face)
-  , _vprops :: Seq v
-  , _hprops :: Map Halfedge h
-  , _eprops :: Map Edge e
-  , _fprops :: Map Face f
+  , _vpoint :: Seq v
+  , _props :: d
   }
 
 makeLenses ''SurfaceMesh
@@ -120,6 +130,7 @@ instance Element Vertex where
   new = Vertex . length . _vconn
 
   conn (Vertex i) = vconn.singular (ix i)
+  propertyOf e = props.property e
 
   hasValidIndex sm (Vertex i) = i < n
     where (VertexSize n) = numElements sm
@@ -132,7 +143,6 @@ instance Element Vertex where
     | h == nullE = True
     | otherwise = any (isBorder sm) (halfedgesAroundTarget sm h)
     where h = halfedge sm v
-
 
   vertex _ v = v
   halfedge sm v = view (conn v.vH) sm
@@ -152,6 +162,7 @@ instance Element Halfedge where
   new = Halfedge . length . _hconn
 
   conn (Halfedge i) = hconn.singular (ix i)
+  propertyOf e = props.property e
 
   hasValidIndex sm (Halfedge i) =
     let (HalfedgeSize n) = numElements sm
@@ -196,6 +207,8 @@ instance Element Edge where
   numElements = EdgeSize . (`div` 2) . length . _hconn
   new = Edge . length . _hconn
 
+  propertyOf e = props.property (div e 2)
+
   hasValidIndex sm (Edge i) = i < n
     where (EdgeSize n) = numElements sm
 
@@ -223,6 +236,7 @@ instance Element Face where
   new = Face . length . _fconn
 
   conn (Face i) = fconn.singular (ix i)
+  propertyOf e = props.property e
 
   hasValidIndex sm (Face i) = i < n
     where (FaceSize n) = numElements sm
@@ -246,91 +260,90 @@ instance Element Face where
     deriving newtype (Bits, Integral, Num, Real)
 
 
-empty :: SurfaceMesh v h e f
+empty :: d -> SurfaceMesh v d
 empty = SurfaceMesh
    Seq.empty Seq.empty Seq.empty
    Seq.empty
-   Map.empty Map.empty Map.empty
 
 -------------------------------------------------------------------------------
 -- Elements
 
-vertices :: SurfaceMesh v h e f -> [Vertex]
+vertices :: SurfaceMesh v d -> [Vertex]
 vertices sm = Prelude.filter (not . isRemoved sm) $
               Vertex <$> [0..fromIntegral (numElements sm :: Size Vertex) - 1]
 
-halfedges :: SurfaceMesh v h e f -> [Halfedge]
+halfedges :: SurfaceMesh v d -> [Halfedge]
 halfedges sm = Prelude.filter (not . isRemoved sm) $
                Halfedge <$> [0..fromIntegral (numElements sm :: Size Halfedge) - 1]
 
-edges :: SurfaceMesh v h e f -> [Edge]
+edges :: SurfaceMesh v d -> [Edge]
 edges sm = Prelude.filter (not . isRemoved sm) $
            Edge . (* 2) <$> [0..fromIntegral (numElements sm :: Size Edge) - 1]
 
-faces :: SurfaceMesh v h e f -> [Face]
+faces :: SurfaceMesh v d -> [Face]
 faces sm = Prelude.filter (not . isRemoved sm) $
            Face <$> [0..fromIntegral (numElements sm :: Size Face) - 1]
 
 -------------------------------------------------------------------------------
 -- Adding elements
 
-addVertex :: SurfaceMesh v h e f -> (Vertex, SurfaceMesh v h e f)
+addVertex :: SurfaceMesh v d -> (Vertex, SurfaceMesh v d)
 addVertex sm =
   let sm' = (vconn._Wrapped') %~ (snoc ?? VertexConnectivity nullE) $ sm
   in (new sm, sm')
 
-addEdge :: SurfaceMesh v h e f -> (Edge, SurfaceMesh v h e f)
+addEdge :: SurfaceMesh v d -> (Edge, SurfaceMesh v d)
 addEdge sm =
   let addh = (snoc ?? HalfedgeConnectivity nullE nullE nullE nullE)
       sm' = (hconn._Wrapped') %~ (addh . addh) $ sm
   in (new sm, sm')
 
-addFace :: SurfaceMesh v h e f -> (Face, SurfaceMesh v h e f)
+addFace :: SurfaceMesh v d -> (Face, SurfaceMesh v d)
 addFace sm =
   let sm' = (fconn._Wrapped') %~ (snoc ?? FaceConnectivity nullE) $ sm
   in (new sm, sm')
 
 -------------------------------------------------------------------------------
--- Removin elements
+-- Removing elements
 
-removeVertex :: SurfaceMesh v h e f -> Vertex -> SurfaceMesh v h e f
+removeVertex :: SurfaceMesh v d -> Vertex -> SurfaceMesh v d
 removeVertex sm v = sm
 
-removeEdge :: SurfaceMesh v h e f -> Edge -> SurfaceMesh v h e f
+removeEdge :: SurfaceMesh v d -> Edge -> SurfaceMesh v d
 removeEdge sm v = sm
 
-removeFace :: SurfaceMesh v h e f -> Face -> SurfaceMesh v h e f
+removeFace :: SurfaceMesh v d -> Face -> SurfaceMesh v d
 removeFace sm v = sm
 
 -------------------------------------------------------------------------------
 -- Connectivity
 
-target :: SurfaceMesh v h e f -> Halfedge -> Vertex
+target :: SurfaceMesh v d -> Halfedge -> Vertex
 target sm h = view (conn h.hV) sm
 
-setTarget :: SurfaceMesh v h e f -> Halfedge -> Vertex -> SurfaceMesh v h e f
+setTarget :: SurfaceMesh v d -> Halfedge -> Vertex -> SurfaceMesh v d
 setTarget sm h v = set (conn h.hV) v sm
 
-setFace :: SurfaceMesh v h e f -> Halfedge -> Face -> SurfaceMesh v h e f
+setFace :: SurfaceMesh v d -> Halfedge -> Face -> SurfaceMesh v d
 setFace sm h f = set (conn h.hF) f sm
 
-setNextOnly :: SurfaceMesh v h e f -> Halfedge -> Halfedge -> SurfaceMesh v h e f
+setNextOnly :: SurfaceMesh v d -> Halfedge -> Halfedge -> SurfaceMesh v d
 setNextOnly sm h nh = set (conn h.hN) nh sm
 
-setPrevOnly :: SurfaceMesh v h e f -> Halfedge -> Halfedge -> SurfaceMesh v h e f
+setPrevOnly :: SurfaceMesh v d -> Halfedge -> Halfedge -> SurfaceMesh v d
 setPrevOnly sm h ph = set (conn h.hP) ph sm
 
-setNext :: SurfaceMesh v h e f -> Halfedge -> Halfedge -> SurfaceMesh v h e f
+setNext :: SurfaceMesh v d -> Halfedge -> Halfedge -> SurfaceMesh v d
 setNext sm h nh = setPrevOnly (setNextOnly sm h nh) nh h
 
-opposite :: SurfaceMesh v h e f -> Halfedge -> Halfedge
+opposite :: SurfaceMesh v d -> Halfedge -> Halfedge
 opposite _ h = xor h 1
 
-source :: SurfaceMesh v h e f -> Halfedge -> Vertex
+source :: SurfaceMesh v d -> Halfedge -> Vertex
 source sm h = target sm (opposite sm h)
 
 
-halfedgeVV :: SurfaceMesh v h e f -> Vertex -> Vertex -> Maybe Halfedge
+halfedgeVV :: SurfaceMesh v d -> Vertex -> Vertex -> Maybe Halfedge
 halfedgeVV sm sour tar =
   assert (hasValidIndex sm sour && hasValidIndex sm tar) result
   where
@@ -345,6 +358,21 @@ halfedgeVV sm sour tar =
       else worker h
 
 -------------------------------------------------------------------------------
+-- Properties
+
+vertexProperties :: Property d Vertex p => SurfaceMesh v d -> Container Vertex p
+vertexProperties sm = properties (_props sm)
+
+halfedgeProperties :: Property d Halfedge p => SurfaceMesh v d -> Container Halfedge p
+halfedgeProperties sm = properties (_props sm)
+
+edgeProperties :: Property d Edge p => SurfaceMesh v d -> Container Edge p
+edgeProperties sm = properties (_props sm)
+
+faceProperties :: Property d Face p => SurfaceMesh v d -> Container Face p
+faceProperties sm = properties (_props sm)
+
+-------------------------------------------------------------------------------
 -- string & error helpers
 
 showConnectivity :: (Show a, Foldable t) => String -> t a -> String
@@ -353,7 +381,7 @@ showConnectivity t s =
   (\i a -> "\n(" ++ show i ++ ", " ++ show a ++ ")") s
 
 conError :: (Element a, Element b, Show a, Show b)
-         => SurfaceMesh v h e f -> a -> String -> b -> String
+         => SurfaceMesh v d -> a -> String -> b -> String
 conError sm els s elt =
   name els ++ " " ++ show els ++ " conneciivity error. " ++
   s ++ " " ++ name elt ++ " " ++ show elt ++ " is " ++
@@ -380,7 +408,7 @@ instance Show (Connectivity Halfedge) where
 instance Show (Connectivity Face) where
   show s = "H " ++ show (_fH s)
 
-instance Show (SurfaceMesh v h e f) where
+instance Show (SurfaceMesh v d) where
   show s = unlines [ showConnectivity "Vertices" (_vconn s)
                    , showConnectivity "Halfedges" (_hconn s)
                    , showConnectivity "Faces" (_fconn s)
@@ -431,11 +459,11 @@ fH g (FaceConnectivity v) = FaceConnectivity <$> g v
 -------------------------------------------------------------------------------
 -- Graph isntances
 
-type instance Graph.VertexDescriptor (SurfaceMesh v h e f) = Vertex
-type instance Graph.HalfedgeDescriptor (SurfaceMesh v h e f) = Halfedge
-type instance Graph.EdgeDescriptor (SurfaceMesh v h e f) = Edge
+type instance Graph.VertexDescriptor (SurfaceMesh v d) = Vertex
+type instance Graph.HalfedgeDescriptor (SurfaceMesh v d) = Halfedge
+type instance Graph.EdgeDescriptor (SurfaceMesh v d) = Edge
 
-instance Graph.HalfedgeGraph (SurfaceMesh v h e f) where
+instance Graph.HalfedgeGraph (SurfaceMesh v d) where
   edge = edge
   halfedgeV = halfedge
   halfedgeE = halfedge
@@ -450,7 +478,7 @@ instance Graph.HalfedgeGraph (SurfaceMesh v h e f) where
   isBorderV = isBorder
   nullHalfedge _ = nullE
 
-instance Graph.MutableHalfedgeGraph (SurfaceMesh v h e f) where
+instance Graph.MutableHalfedgeGraph (SurfaceMesh v d) where
   addVertex = addVertex
   removeVertex = removeVertex
   addEdge = addEdge
@@ -459,29 +487,29 @@ instance Graph.MutableHalfedgeGraph (SurfaceMesh v h e f) where
   setNext = setNext
   setHalfedgeV = setHalfedge
 
-type instance Graph.FaceDescriptor (SurfaceMesh v h e f) = Face
+type instance Graph.FaceDescriptor (SurfaceMesh v d) = Face
 
-instance Graph.FaceGraph (SurfaceMesh v h e f) where
+instance Graph.FaceGraph (SurfaceMesh v d) where
   face = face
   halfedgeF = halfedge
 
   nullFace _ = nullE
 
-instance Graph.MutableFaceGraph (SurfaceMesh v h e f) where
+instance Graph.MutableFaceGraph (SurfaceMesh v d) where
   addFace = addFace
   removeFace = removeFace
   setFace = setFace
   setHalfedgeF = setHalfedge
 
-type instance GraphM.VertexDescriptor (SurfaceMesh v h e f) = Vertex
-type instance GraphM.HalfedgeDescriptor (SurfaceMesh v h e f) = Halfedge
-type instance GraphM.EdgeDescriptor (SurfaceMesh v h e f) = Edge
+type instance GraphM.VertexDescriptor (SurfaceMesh v d) = Vertex
+type instance GraphM.HalfedgeDescriptor (SurfaceMesh v d) = Halfedge
+type instance GraphM.EdgeDescriptor (SurfaceMesh v d) = Edge
 
-type St v h e f = State (SurfaceMesh v h e f)
+type St v d = State (SurfaceMesh v d)
 
--- instance GraphM.HalfedgeGraphS (St v h e f) (SurfaceMesh v h e f)
+-- instance GraphM.HalfedgeGraphS (St v h e f) (SurfaceMesh v d)
 
-instance GraphM.HalfedgeGraph (St v h e f) (SurfaceMesh v h e f) where
+instance GraphM.HalfedgeGraph (St v d) (SurfaceMesh v d) where
   edge _ h = gets (`edge` h)
   halfedgeV _ h = gets (`halfedge` h)
   halfedgeE _ h = gets (`halfedge` h)
@@ -498,7 +526,7 @@ instance GraphM.HalfedgeGraph (St v h e f) (SurfaceMesh v h e f) where
 
   showM _ = gets show
 
-instance GraphM.MutableHalfedgeGraph (St v h e f) (SurfaceMesh v h e f) where
+instance GraphM.MutableHalfedgeGraph (St v d) (SurfaceMesh v d) where
   addVertex _ = state addVertex
   removeVertex _ v = modify (`removeVertex` v)
   addEdge _ = state addEdge
@@ -507,15 +535,15 @@ instance GraphM.MutableHalfedgeGraph (St v h e f) (SurfaceMesh v h e f) where
   setNext _ h1 h2 = modify (\s -> setNext s h1 h2)
   setHalfedgeV _ v h = modify (\s -> setHalfedge s v h)
 
-type instance GraphM.FaceDescriptor (SurfaceMesh v h e f) = Face
+type instance GraphM.FaceDescriptor (SurfaceMesh v d) = Face
 
-instance GraphM.FaceGraph (St v h e f) (SurfaceMesh v h e f) where
+instance GraphM.FaceGraph (St v d) (SurfaceMesh v d) where
   face _ h = gets (`face` h)
   halfedgeF _ h = gets (`halfedge` h)
 
   nullFace _ = return nullE
 
-instance GraphM.MutableFaceGraph (St v h e f) (SurfaceMesh v h e f) where
+instance GraphM.MutableFaceGraph (St v d) (SurfaceMesh v d) where
   addFace _ = state addFace
   removeFace _ f = modify (`removeFace` f)
   setFace _ h f = modify (\s -> setFace s h f)
@@ -524,10 +552,10 @@ instance GraphM.MutableFaceGraph (St v h e f) (SurfaceMesh v h e f) where
 -------------------------------------------------------------------------------
 -- Garbage temp tests
 
-foo :: SurfaceMesh () () () ()
+foo :: SurfaceMesh () ()
 foo =
-  let sm = empty
-      f :: St () () () () ()
+  let sm = empty ()
+      f :: St () () ()
       f = do
             v1 <- GraphM.addVertex sm
             v2 <- GraphM.addVertex sm
@@ -536,12 +564,25 @@ foo =
             return ()
   in execState f sm
 
-foo2 :: SurfaceMesh () () () ()
+newtype MyProps2 = MyProps2 (IntMap Int, IntMap (Map String String))
+
+instance Property MyProps2 Vertex Int where
+  type Container Vertex = IntMap
+  property (Vertex i) g (MyProps2 m) = MyProps2 <$> (_1.at i) g m
+  properties (MyProps2 m) = fst m
+
+instance Property MyProps2 Edge (Map String String) where
+  type Container Edge = IntMap
+  property (Edge i) g (MyProps2 m) = MyProps2 <$> (_2.at i) g m
+  properties (MyProps2 m) = snd m
+
+foo2 :: SurfaceMesh () MyProps2
 foo2 =
-  let sm = empty
-      f :: St () () () () ()
+  let sm = empty (MyProps2 (IntMap.empty, IntMap.empty))
       f = do
             v1 <- GraphM.addVertex sm
             GraphM.addEdge sm
-            return ()
+            propertyOf (Vertex 0) ?= 7
+            propertyOf (Edge 0) ?= Map.fromList [("foo", "bar")]
+            (propertyOf (Edge 0)._Just.at "foo") ?= "alice"
   in execState f sm
